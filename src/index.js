7d870fa200d1d34cc343b57401cdd4589590ed8a
Marcus Pagum <marcuspagum@gmail.com>
2021-04-08 20:24:27 -0300

    Desafio middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next();
+  }
+
+  if (user.todos.length < 10) {
+    return next()
+  }
+
+  return response.status(403).json({ error: 'Max number of todos. Join the Todos-PRO to have access to unlimited todos'})
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+
+  const todo = user.todos.find((userTodo) => userTodo.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  if (!validate(todo.id)) {
+    return response.status(404).json({ error: 'Todo is not uuid'})
+  }
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Uuid not found'})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
